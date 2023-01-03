1fce94ea67251452739d10bef3a7efbf7c575768
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2023-01-03 14:18:56 -0300

    updated utils functions

--- utils.py
+++ utils.py
@@ -9,8 +9,11 @@ def require_keys(d, keys):
             raise ValueError(f"'{k}' must be set".format(k))
     return True
 
-
 def encode_base64(text):
     message_bytes = text.encode("ascii")
     base64_bytes = base64.b64encode(message_bytes)
     return base64_bytes.decode("ascii")
+
+def calculate_identity_id(verification_id):
+    identity_id = hex(int(verification_id, 16) - 2)[2:]
+    return identity_id
